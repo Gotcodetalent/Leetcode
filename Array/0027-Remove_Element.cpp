547e225210a5509f71ff6a0d883dae60f58ecde0
WCCCYCC <116697928+WCCCYCC@users.noreply.github.com>
2023-12-21 16:27:44 +0800

    Update 0027-Remove_Element.cpp

--- Array/0027-Remove_Element.cpp
+++ Array/0027-Remove_Element.cpp
@@ -1,12 +1,30 @@
 /*
 題目：
-
+https://leetcode.com/problems/remove-element/description/
+與26題類似, 但必須移除值與val相同的數值
 
 解說：
-
+使用idx來指向目前目前的k-value,從0開始,並linear scan目前的array中有那些值不是val, 將其依據idx更新,並更新idx值
 
 有使用到的觀念：
-
+Array, TwoPointer
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.size() == 0) return NULL;
+        int idx = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] != val)
+            {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+
+        return idx;
+    }
+};
