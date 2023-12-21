7f0ba176c5fd3576b7c4ab297ebbeaa534a1246d
WCCCYCC <116697928+WCCCYCC@users.noreply.github.com>
2023-12-21 14:48:32 +0800

    Update 0026-Remove_Duplicates_from_Sorted_Array.cpp

--- Array/0026-Remove_Duplicates_from_Sorted_Array.cpp
+++ Array/0026-Remove_Duplicates_from_Sorted_Array.cpp
@@ -6,7 +6,7 @@ https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 給定一個sorted in non-decreasing order的整數陣列,in-place的移除重複的元素,並返回value k *移除後的陣列之前k個元素就是所有元素的集合*。 
 
 有使用到的觀念：
-Binary-search
+Array, two pointers
 */
 
 // #include "../code_function.h"
