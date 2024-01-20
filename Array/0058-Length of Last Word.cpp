5f325123849bec6329ee1d28460dd4640d06473a
WCCCYCC <116697928+WCCCYCC@users.noreply.github.com>
2024-01-20 10:27:47 +0800

    Update 0058-Length of Last Word.cpp

--- Array/0058-Length of Last Word.cpp
+++ Array/0058-Length of Last Word.cpp
@@ -3,10 +3,19 @@
 https://leetcode.com/problems/length-of-last-word/
 
 解說：
-
+題目要找出最後一個substring的長度，所以先從最右邊移除空格，接著算出子字串的長度。
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size(), length = 0;
+        while(n >= 0 && s[--n] == ' ' );
+        while(n >= 0 && s[n--] != ' ') length++;
+        return length;
+    }
+};
