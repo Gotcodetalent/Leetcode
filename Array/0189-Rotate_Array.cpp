33a0260fb6dedb836687508b016c46431601ee0c
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-09-08 18:43:36 +0800

    Update 0189-Rotate_Array.cpp

--- Array/0189-Rotate_Array.cpp
+++ Array/0189-Rotate_Array.cpp
@@ -4,10 +4,28 @@ https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&en
 
 
 解說：
-
+會平移k%n格,所以原本在位置i,最後會落在(i+k)%n的位置
+先copy在一個vector,最後assign
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k == 0) return;
+        const int n = nums.size();
+        vector<int> copy(n,0);
+        for(int i=0; i<n; i++)
+        {
+            copy[(i+k)%n] = nums[i];
+        }
+        for(int i =0;i<n; i++)
+        {
+            nums[i]=copy[i];
+        }
+        return;
+    }
+};
