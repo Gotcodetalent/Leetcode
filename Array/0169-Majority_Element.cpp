bae3a2e150ac7021bf93131682b51757b5646c88
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-09-08 17:54:58 +0800

    Update 0169-Majority_Element.cpp

--- Array/0169-Majority_Element.cpp
+++ Array/0169-Majority_Element.cpp
@@ -6,7 +6,24 @@ https://leetcode.com/problems/majority-element/description/
 
 
 有使用到的觀念：
-
+Array, Hash
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        const int n = nums.size();
+        if(n < 2) return nums[0];
+
+        unordered_map<int, int> dp;
+        for(int i=0; i<n; i++)
+        {
+            dp[nums[i]]++;
+            if(dp[nums[i]] > n/2) return nums[i];
+        }
+
+        return -1;
+    }
+};
