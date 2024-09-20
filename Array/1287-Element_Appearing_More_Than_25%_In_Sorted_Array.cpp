822b4342f5a6662377a4eac8caf1a958397a9635
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-09-20 16:00:43 +0800

    Update 1287-Element_Appearing_More_Than_25%_In_Sorted_Array.cpp

--- Array/1287-Element_Appearing_More_Than_25%_In_Sorted_Array.cpp
+++ Array/1287-Element_Appearing_More_Than_25%_In_Sorted_Array.cpp
@@ -1,6 +1,6 @@
 /*
 題目：
-https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/description/
 
 解說：
 
