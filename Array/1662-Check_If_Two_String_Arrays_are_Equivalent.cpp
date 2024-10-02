e12469ddb13803e63b4a4af5b6629f0beb84e266
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-02 16:13:44 +0800

    Update 1662-Check_If_Two_String_Arrays_are_Equivalent.cpp

--- Array/1662-Check_If_Two_String_Arrays_are_Equivalent.cpp
+++ Array/1662-Check_If_Two_String_Arrays_are_Equivalent.cpp
@@ -10,3 +10,20 @@ https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/descript
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int n1 = word1.size(), n2 = word2.size();
+        string s1 = "", s2 = "";
+        for(int i=0; i<n1; i++) {
+            s1 += word1[i];
+        }        
+        for(int i=0; i<n2; i++) {
+            s2 += word2[i];
+        }
+
+        bool ret = s1.compare(s2) == 0 ? 1 : 0;
+
+        return ret;
+    }
+};
