ff3f8385f9a0d62c58ef216ac76e85d43d9daf88
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-03 16:44:13 +0800

    Update 1980-Find_Unique_Binary_String.cpp

--- Array/1980-Find_Unique_Binary_String.cpp
+++ Array/1980-Find_Unique_Binary_String.cpp
@@ -3,10 +3,37 @@
 https://leetcode.com/problems/find-unique-binary-string/description/
 
 解說：
-
+找出給定字串陣列中沒有出現過的任意一個排列
 
 有使用到的觀念：
-
+DFS
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    unordered_map<string,int> unique;
+    string ret="";
+    bool found = false;
+    void dfs(int n, string s) {
+        if(found) return;
+        if(n == 0) {
+            if(unique.find(s) == unique.end()) {
+                ret = s;
+                found = true;
+            }
+            return;
+        }
+        dfs(n-1, s+"0");
+        dfs(n-1, s+"1");
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        const int n = nums[0].size();
+        for (const auto& num : nums) {
+            unique.emplace(num, 1);
+        }
+        string s ="";
+        dfs(n, s);
+        return ret;
+    }
+};
