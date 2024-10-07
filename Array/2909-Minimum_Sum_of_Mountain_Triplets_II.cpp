91676eb413ac635094597b459e3aeb5855ca9809
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-08 00:03:43 +0800

    Update 2909-Minimum_Sum_of_Mountain_Triplets_II.cpp

--- Array/2909-Minimum_Sum_of_Mountain_Triplets_II.cpp
+++ Array/2909-Minimum_Sum_of_Mountain_Triplets_II.cpp
@@ -3,10 +3,32 @@
 https://leetcode.com/problems/minimum-sum-of-mountain-triplets-ii/
 
 解說：
-
+同2908,但MAX不同
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minimumSum(vector<int>& nums) {
+        const int n = nums.size();
+        const int MAX = 100000000;
+        vector<int> I(n, MAX);
+        vector<int> K(n, MAX);
+        int sum = INT_MAX;
+        for(int j=1; j<n-1; j++)
+        {
+            I[j] = min(I[j-1], nums[j-1]);
+            K[n-j-1] = min(K[n-j], nums[n-j]);
+        }
+        for(int j=1; j<n-1; j++)
+        {
+            if(I[j] < nums[j] && K[j] < nums[j]) sum = min(sum, I[j] + K[j] + nums[j]);
+        }
+
+        if(sum == INT_MAX) return -1;
+        else return sum; 
+    }
+};
