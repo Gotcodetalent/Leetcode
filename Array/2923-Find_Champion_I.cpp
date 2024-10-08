0a3aa0cee0d38ddcfb2cc28ea63082cc48c259d6
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-09 01:19:27 +0800

    Update 2923-Find_Champion_I.cpp

--- Array/2923-Find_Champion_I.cpp
+++ Array/2923-Find_Champion_I.cpp
@@ -3,10 +3,23 @@
 https://leetcode.com/problems/find-champion-i/description/
 
 解說：
-
+有n個team, 比賽結果只有win or lose,所以只要利用查表結果比較n-1次,就可以知道誰是champion (比較過程中贏的就設定為champ)
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        const int n = grid.size();;
+        int champ = 0;
+        for(int i=1; i<n ;i++)
+        {
+            if(grid[champ][i] == 0) champ = i;
+        }
+
+        return champ;
+    }
+};
