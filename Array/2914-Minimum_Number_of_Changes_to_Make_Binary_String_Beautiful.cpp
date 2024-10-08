5576f272e772fd4406d0455abc9139948424c856
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-09 00:35:33 +0800

    Update 2914-Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.cpp

--- Array/2914-Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.cpp
+++ Array/2914-Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.cpp
@@ -3,10 +3,24 @@
 https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful/
 
 解說：
-
+s長度為even, 且符合的substr切割成最小單位後長度為2,因此只要兩兩判斷字元是否相同即可 ("00", "11")
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minChanges(string s) {
+        const int n = s.size();
+        int sum = 0;
+        for(int i=1; i<n; i+=2)
+        {
+            if(s[i] == s[i-1]) continue;
+            else sum++;
+        }
+
+        return sum;
+    }
+};
