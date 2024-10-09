dff19bf1678ac9cbe7000e3cff475413f5c1ac10
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-09 17:00:51 +0800

    Update 0102-Binary_Tree_Level_Order_Traversal.cpp

--- BFS/0102-Binary_Tree_Level_Order_Traversal.cpp
+++ BFS/0102-Binary_Tree_Level_Order_Traversal.cpp
@@ -5,7 +5,7 @@ https://leetcode.com/problems/binary-tree-level-order-traversal/
 解說：
 利用BFS求level order,首先插入第一個節點,當queue不為null時,代表該level有可以輸出的order
 對於每一層的每個非空節點 x,將其value插入tmp中,接著將x->left, x->right插入queue中,探索完後刪除 x。
-由tmp所組成的ans即為所有level的order
+組合每個level的order即為ans 
 
 有使用到的觀念：
 BFS, Tree
