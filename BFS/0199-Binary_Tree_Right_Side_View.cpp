8727ea81a4a8a197cbb553aeb14347a1e0586b4c
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-15 17:58:00 +0800

    Update 0199-Binary_Tree_Right_Side_View.cpp

--- BFS/0199-Binary_Tree_Right_Side_View.cpp
+++ BFS/0199-Binary_Tree_Right_Side_View.cpp
@@ -3,10 +3,46 @@
 https://leetcode.com/problems/binary-tree-right-side-view/
 
 解說：
-
+與0102 level order概念相同,只是變成要印出最右端,即每次BFS時的最後一個節點的val
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if(root == nullptr) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            for(int i=0; i<n; i++)
+            {
+                TreeNode* node = q.front();
+                if(node -> left != NULL) q.push(node -> left);
+                if(node -> right != NULL) q.push(node -> right);
+
+                if(i == n-1) ans.push_back(node -> val);
+                q.pop();
+            }
+        }
+
+        return ans;
+    }
+};
