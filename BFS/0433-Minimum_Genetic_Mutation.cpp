24a4749cf9495251fc26aef390c6ef0a080dcf04
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-15 21:58:48 +0800

    Update 0433-Minimum_Genetic_Mutation.cpp

--- BFS/0433-Minimum_Genetic_Mutation.cpp
+++ BFS/0433-Minimum_Genetic_Mutation.cpp
@@ -3,10 +3,51 @@
 https://leetcode.com/problems/minimum-genetic-mutation/
 
 解說：
-
+與0127 Word_Ladder概念相同
 
 有使用到的觀念：
-
+BFS
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        unordered_set<string> dict(bank.begin(), bank.end());
+        if(!dict.count(endGene)) return -1;
+
+        queue<string> q;
+        vector<char> genes = {'A','C','T','G'};
+        q.push(startGene);
+        int mM = 0;
+        while(!q.empty())
+        {
+            int n = q.size();
+            for(int i=0; i<n; i++)
+            {
+                string gene = q.front();
+                q.pop();
+                for(int j=0; j<gene.size(); j++)
+                {
+                    char ch = gene[j];
+                    for(char g: genes)
+                    {
+                        gene[j] = g;
+                        if(gene == endGene) return mM + 1;
+                        if(dict.find(gene) != dict.end())
+                        {
+                            dict.erase(gene);
+                            q.push(gene);
+                        }
+                    }
+                    gene[j] = ch;
+                }
+
+            }
+            mM++;
+        }
+
+        return -1;
+        
+    }
+};
