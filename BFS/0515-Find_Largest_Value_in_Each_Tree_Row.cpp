2381fb0b7c701c9cabc1f7189b359a15e4725e80
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-16 00:12:24 +0800

    Update 0515-Find_Largest_Value_in_Each_Tree_Row.cpp

--- BFS/0515-Find_Largest_Value_in_Each_Tree_Row.cpp
+++ BFS/0515-Find_Largest_Value_in_Each_Tree_Row.cpp
@@ -3,10 +3,47 @@
 https://leetcode.com/problems/find-largest-value-in-each-tree-row/description/
 
 解說：
-
+BFS加入max值的比較
 
 有使用到的觀念：
-
+BFS
 */
 
 // #include "../code_function.h"
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> ans;
+        if(root == nullptr) return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int max = INT_MIN;
+            int n = q.size();
+            for(int i=0; i<n; i++)
+            {
+                TreeNode* node = q.front();
+                if(node -> val > max) max = node -> val;
+                if(node -> left != NULL) q.push(node -> left);
+                if(node -> right != NULL) q.push(node -> right);
+                q.pop();
+            }
+            ans.push_back(max);
+        }
+
+        return ans;
+    }
+};
