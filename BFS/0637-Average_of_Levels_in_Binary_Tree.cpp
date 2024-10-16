ef0d6c2a78c0fb850dec6a38ae3a5f6a16ab050a
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-16 16:54:21 +0800

    Update 0637-Average_of_Levels_in_Binary_Tree.cpp

--- BFS/0637-Average_of_Levels_in_Binary_Tree.cpp
+++ BFS/0637-Average_of_Levels_in_Binary_Tree.cpp
@@ -3,10 +3,34 @@
 https://leetcode.com/problems/average-of-levels-in-binary-tree/description
 
 解說：
-
+求Binary tree每個level元素的avg
 
 有使用到的觀念：
-
+BFS
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            double sum = 0;
+            for(int i=0; i<n; i++)
+            {
+                TreeNode* node = q.front();
+                sum += node->val;
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
+                q.pop();
+            }
+            ans.push_back(sum/n);
+        }
+        
+        return ans;
+    }
+};
