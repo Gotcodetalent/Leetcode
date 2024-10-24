51a0d381770eb5752ab796436a648f332618a7ae
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-24 12:43:07 +0800

    Update 1466-Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp

--- BFS/1466-Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp
+++ BFS/1466-Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp
@@ -10,3 +10,37 @@ https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        unordered_map<int, vector<pair<int,bool>>> neighbors;
+        for(auto& connect : connections)
+        {
+            neighbors[connect[0]].push_back({connect[1],true}); //need to change direction
+            neighbors[connect[1]].push_back({connect[0],false});
+        }
+
+        vector<bool> reach(n, false);
+        queue<int> q;
+        q.push(0);
+        reach[0] = true;
+        int change = 0;
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for(auto& neighbor : neighbors[node])
+            {
+                if(!reach[neighbor.first])
+                {
+                    reach[neighbor.first] = true;
+                    q.push(neighbor.first);
+                    if(neighbor.second) ++change;
+                }
+            }
+        }
+
+        return change;
+    }
+};
