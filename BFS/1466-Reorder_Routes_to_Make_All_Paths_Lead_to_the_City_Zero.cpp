4dba650daebc06eb9eaed26b0767ffc62d1d66d3
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-24 12:59:40 +0800

    Update 1466-Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp

--- BFS/1466-Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp
+++ BFS/1466-Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp
@@ -3,7 +3,10 @@
 https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
 
 解說：
+先遍歷所有的node,將邊的方向確定, 因為從 node 0 開始BFS, 最終所有的點都要導向 0
+所以對於任一edge(a, b), b -> a為不用調整的方向
 
+接著BFS所有沒拜訪過的node, 若遇到反向邊則增加change次數
 
 有使用到的觀念：
 
