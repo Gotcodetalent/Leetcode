66f5e9a271a4b097fa6c5177dc6c52d8765c61c4
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-24 17:20:21 +0800

    Update 0022-Generate_Parentheses.cpp

--- DFS/0022-Generate_Parentheses.cpp
+++ DFS/0022-Generate_Parentheses.cpp
@@ -3,10 +3,29 @@
 https://leetcode.com/problems/generate-parentheses/
 
 解說：
-
+右括弧數量不可大於左括弧,字串長度不大於2n
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    vector<string> ans;
+
+    void dfs(int n, int l, int r, string tmp) {
+        if (r > l || l > n || r > n) return; 
+        if (l == n && r == n) { // 如果已生成所有括號對
+            ans.push_back(tmp);
+            return;
+        }
+        dfs(n, l + 1, r, tmp + '('); // 加入左括號
+        dfs(n, l, r + 1, tmp + ')'); // 加入右括號
+    }
+
+    vector<string> generateParenthesis(int n) {
+        dfs(n, 0, 0, "");
+        return ans;
+    }
+};
