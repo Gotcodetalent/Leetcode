aba1d69e60f917a3e014547a19eaacc1f0d693af
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-24 16:36:43 +0800

    Update 0017-Letter_Combinations_of_a_Phone_Number.cpp

--- DFS/0017-Letter_Combinations_of_a_Phone_Number.cpp
+++ DFS/0017-Letter_Combinations_of_a_Phone_Number.cpp
@@ -10,3 +10,37 @@ https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    unordered_map<char, vector<char>> ump;
+    vector<string> ans;
+    int n = 0;
+    void dfs(string digits, int curr, string tmp){
+        if(curr == n)
+        {
+            ans.push_back(tmp);
+            return;
+        }
+        for(auto& ch : ump[digits[curr]])
+        {
+            dfs(digits,curr + 1, tmp+ch);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        n = digits.length();
+        if(n == 0) return {};
+        ump['2'] = {'a','b','c'};
+        ump['3'] = {'d','e','f'};
+        ump['4'] = {'g','h','i'};
+        ump['5'] = {'j','k','l'};
+        ump['6'] = {'m','n','o'};
+        ump['7'] = {'p','q','r','s'};
+        ump['8'] = {'t','u','v'};
+        ump['9'] = {'w','x','y','z'};
+        
+        dfs(digits,0,"");
+
+        return ans;
+    }
+};
