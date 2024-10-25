acaa9bd8e9d9b116b58ebdf446ffbd58e8f010f9
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-25 23:28:08 +0800

    Update 0079-Word_Search.cpp

--- DFS/0079-Word_Search.cpp
+++ DFS/0079-Word_Search.cpp
@@ -12,29 +12,29 @@ https://leetcode.com/problems/word-search/description/
 // #include "../code_function.h"
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, int x, int y, string word, int start, vector<vector<bool>>& visited) {
+    bool dfs(vector<vector<char>>& board, int x, int y, string word, int start) {
         if(start == word.length()) return true;
         vector<int> dir = {1,0,-1,0,1};
-        visited[x][y] = true;
+        char temp = board[x][y];
+        board[x][y] = '#';
         for(int i=0; i<4; i++){
             int xn = x + dir[i];
             int yn = y + dir[i+1];
             if(xn < 0 || yn < 0 || xn >= board.size() || yn >= board[0].size()) continue;
-            if(board[xn][yn] == word[start] && !visited[xn][yn]) {
-                if(dfs(board,xn, yn, word, start+1, visited)) return true; 
+            if(board[xn][yn] == word[start] && board[xn][yn] != '#') {
+                if(dfs(board,xn, yn, word, start+1)) return true; 
             }
         }
 
-        visited[x][y] = false;
+        board[x][y] = temp;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if((board[i][j] == word[0]) && dfs(board,i,j,word,1,visited)) return true;
+                if((board[i][j] == word[0]) && dfs(board,i,j,word,1)) return true;
             }
         }
 
