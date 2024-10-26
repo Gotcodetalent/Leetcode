ec3fac94d4949a6b702edd52821fe7e3031141bb
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-26 20:02:58 +0800

    Update 0200-Number_of_Islands.cpp

--- DFS/0200-Number_of_Islands.cpp
+++ DFS/0200-Number_of_Islands.cpp
@@ -10,3 +10,33 @@ https://leetcode.com/problems/number-of-islands/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int dfs(int x, int y, vector<vector<char>>& grid) {
+        grid[x][y] = '0';
+        vector<int> dir = {1,0,-1,0,1};
+        for(int i=0; i<4; i++)
+        {
+            int xn = x + dir[i];
+            int yn = y + dir[i+1];
+            if(xn < 0 || yn < 0 || xn >= grid.size() || yn >= grid[0].size()) continue; 
+            if(grid[xn][yn] == '1') {
+                int tmp = dfs(xn,yn,grid);
+            }
+        }
+
+        return 1;
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int ans = 0;
+        for(int i=0; i<grid.size();i++)
+        {
+            for(int j=0; j<grid[0].size(); j++)
+            {
+                if(grid[i][j] == '1') ans += dfs(i,j,grid);
+            }
+        }
+        return ans;
+    }
+};
