66c6cd11324e5d1c291575891657cbb256048153
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-27 19:22:19 +0800

    Update 0687-Longest_Univalue_Path.cpp

--- DFS/0687-Longest_Univalue_Path.cpp
+++ DFS/0687-Longest_Univalue_Path.cpp
@@ -10,3 +10,26 @@ https://leetcode.com/problems/longest-univalue-path/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int LongestPath = 0;
+
+    int dfs(TreeNode* root) {
+        if(root == nullptr) {
+            return 0;
+        }
+        int l = dfs(root -> left);
+        int r = dfs(root -> right);
+        int lcurr = 0, rcurr = 0;
+        if(root -> left && (root -> left) -> val == root -> val) lcurr = l + 1;
+        if(root -> right && (root -> right) -> val == root -> val) rcurr = r + 1;
+        LongestPath = max(LongestPath, lcurr + rcurr);
+
+        return max(lcurr, rcurr);
+    }
+
+    int longestUnivaluePath(TreeNode* root) {
+        dfs(root);
+        return LongestPath;
+    }
+};
