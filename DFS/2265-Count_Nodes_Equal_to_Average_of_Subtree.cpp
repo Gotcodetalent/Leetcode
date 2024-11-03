aa7978bc3f9b52819b0496761c1f9c36a7d185a8
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-11-03 16:21:10 +0800

    Update 2265-Count_Nodes_Equal_to_Average_of_Subtree.cpp

--- DFS/2265-Count_Nodes_Equal_to_Average_of_Subtree.cpp
+++ DFS/2265-Count_Nodes_Equal_to_Average_of_Subtree.cpp
@@ -3,10 +3,32 @@
 https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
 
 解說：
-
+DFS中使用struct同時記錄左右子樹的size跟sum, 只要node與avg相同便++ans;
 
 有使用到的觀念：
 
 */
+class Solution {
+public:
+    int ans = 0;
+    struct subtree{
+        int sum;
+        int size;
+    };
+    subtree dfs(TreeNode* node) {
+        if(node == nullptr) return {0,0};
 
-// #include "../code_function.h"
+        subtree l = dfs(node -> left);
+        subtree r = dfs(node -> right);
+        int sum = l.sum + r.sum + node->val;
+        int size = l.size + r.size + 1; 
+        if((sum / size) == node->val) ++ans;
+
+        return {sum, size};
+    } 
+
+    int averageOfSubtree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
