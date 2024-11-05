a9c63fe97d51006ee4a05de7d1fc9f5aa28844aa
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-11-05 22:56:51 +0800

    Update 0070-Climbing_Stairs.cpp

--- DP/0070-Climbing_Stairs.cpp
+++ DP/0070-Climbing_Stairs.cpp
@@ -3,10 +3,20 @@
 https://leetcode.com/problems/climbing-stairs/
 
 解說：
-
+一次爬1 or 2格, 求有幾種方法爬到n格
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        for(int i=2; i<n+1 ;i++) dp[i] += dp[i-1] + dp[i-2];
+        
+        return dp[n];
+    }
+};
