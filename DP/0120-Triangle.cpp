7bd2fc3442501dda67bb05b5445cb7dcca3fcd5e
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-11-08 00:08:23 +0800

    Update 0120-Triangle.cpp

--- DP/0120-Triangle.cpp
+++ DP/0120-Triangle.cpp
@@ -3,10 +3,30 @@
 https://leetcode.com/problems/triangle/
 
 解說：
-
+dp(i, j)的定義為,當move到(row, col) = (i, j)時, 其最小值為多少
+若要move到(i, j)上個位置會是(i-1, j)、(i-1, j-1)
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        const int n = triangle.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1,INT_MAX/2));
+        dp[1][1] = triangle[0][0];
+        for(int i=2; i<=n; i++)
+        {
+            for(int j=1; j<=triangle[i-1].size(); j++)
+            {
+                dp[i][j] = min(dp[i-1][j] + triangle[i-1][j-1], dp[i-1][j-1] + triangle[i-1][j-1]);
+            }
+        }
+        
+        int min_sum = INT_MAX;
+        for(int sum : dp[n]) min_sum = min(min_sum, sum);
+        return min_sum ;
+    }
+};
