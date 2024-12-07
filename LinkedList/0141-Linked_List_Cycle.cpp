381f143d60158958276ad379bd8c2e472d9a70e4
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-07 21:06:38 +0800

    Update 0141-Linked_List_Cycle.cpp

--- LinkedList/0141-Linked_List_Cycle.cpp
+++ LinkedList/0141-Linked_List_Cycle.cpp
@@ -3,10 +3,26 @@
 https://leetcode.com/problems/linked-list-cycle/
 
 解說：
-
+快慢指標, 只要slow與fast能夠相遇, 就代表有cycle
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) return false;
+        ListNode *slow = head, *fast = head->next;
+        while(fast && fast->next)
+        {
+            if(slow == fast) return true;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return false;
+    }
+};
