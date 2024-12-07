30f290e442d8cfebfd1667509d5bbdf760e15d28
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-07 20:23:55 +0800

    Update 0138-Copy_List_with_Random_Pointer.cpp

--- LinkedList/0138-Copy_List_with_Random_Pointer.cpp
+++ LinkedList/0138-Copy_List_with_Random_Pointer.cpp
@@ -3,10 +3,55 @@
 https://leetcode.com/problems/copy-list-with-random-pointer/description/
 
 解說：
-
+clone 的套路就是使用 unordered_map, 以原始 node 為 index 來查找對應 copy node 的 Hash map
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == nullptr) return nullptr;
+
+        Node* clone = new Node(-1);
+        Node* copy = clone;
+        Node* curr = head;
+        unordered_map<Node*,Node*> ump;
+        while(curr)
+        {
+            Node* tmp = new Node(curr->val);
+            ump[curr] = tmp; //以curr作為index mapping new list's node
+            copy->next = tmp;
+            copy = copy->next;
+            curr = curr->next;
+        }
+
+        copy = clone->next;
+        curr = head;
+
+        while(curr) //對random pointer copy
+        {
+            copy->random = ump[curr->random];
+            copy = copy->next;
+            curr = curr->next;
+        }
+
+        return clone->next;
+    }
+};
