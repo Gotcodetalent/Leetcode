050d3241fac39f8a4b8e0f796b3f66c5023f1ad0
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-10 00:40:50 +0800

    Update 1984-Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.cpp

--- Greedy/1984-Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.cpp
+++ Greedy/1984-Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.cpp
@@ -10,3 +10,16 @@ https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int mx = INT_MAX/2;
+        for(int i=0; i<nums.size()-k+1; i++)
+        {
+            mx = min(mx,nums[i+k-1] - nums[i]);
+        }
+
+        return mx;
+    }
+};
