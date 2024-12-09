322b35570e89a1a6fa709365fc1dbcb71ae461ea
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-10 01:43:34 +0800

    Update 2856-Minimum_Array_Length_After_Pair_Removals.cpp

--- Greedy/2856-Minimum_Array_Length_After_Pair_Removals.cpp
+++ Greedy/2856-Minimum_Array_Length_After_Pair_Removals.cpp
@@ -3,10 +3,33 @@
 https://leetcode.com/problems/minimum-array-length-after-pair-removals/
 
 解說：
-
+從頭及中間開始兩兩一對測試
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minLengthAfterRemovals(vector<int>& nums) {
+        const int n = nums.size();
+        int ans = n;
+        int p1 = 0, p2 = n/2;
+        while(p2 < n)
+        {
+            if(nums[p1] < nums[p2]) {
+                ans-=2;
+                ++p1;
+                ++p2;
+            }
+            else {
+                ++p2;
+            }
+
+            if(p1 >= n/2) break; //能移除的pair都已確認過了
+        }
+
+        return ans;
+    }
+};
