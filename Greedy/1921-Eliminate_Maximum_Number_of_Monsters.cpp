6565339f18f819940e43a10f4142a54ee294149e
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-10 00:22:55 +0800

    Update 1921-Eliminate_Maximum_Number_of_Monsters.cpp

--- Greedy/1921-Eliminate_Maximum_Number_of_Monsters.cpp
+++ Greedy/1921-Eliminate_Maximum_Number_of_Monsters.cpp
@@ -10,3 +10,23 @@ https://leetcode.com/problems/eliminate-maximum-number-of-monsters/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        const int n = dist.size();
+        vector<double> round(n); //怪獸距離戰場幾回合的時間
+        for(int i=0; i<n; i++)
+        {
+            round[i] = static_cast<double>(dist[i]) /speed[i];
+        } 
+
+        sort(round.begin(), round.end());
+
+        for(int i=0; i<n; i++) //開始殺怪獸,第i回合可以處理 round 大於 i 的怪獸 
+        {
+            if(round[i] <= i) return i;
+        }
+
+        return n;
+    }
+};
