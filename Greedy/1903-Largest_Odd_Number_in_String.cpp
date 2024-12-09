4cd9dc62d1981477da87eb439b7d059eb6174c15
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-09 22:02:52 +0800

    Update 1903-Largest_Odd_Number_in_String.cpp

--- Greedy/1903-Largest_Odd_Number_in_String.cpp
+++ Greedy/1903-Largest_Odd_Number_in_String.cpp
@@ -4,10 +4,31 @@ https://leetcode.com/problems/largest-odd-number-in-string/
 
 
 解說：
-
+先找到最小的奇數所在的位置
 
 有使用到的觀念：
+Greedy
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        const int n = num.length();
+        string ans = "";
+        bool findOdd = false;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(num[i] % 2 == 1 && !findOdd) findOdd = true;
+            if(findOdd) 
+            {
+                ans = num.substr(0,i+1);
+                return ans;
+            }
+            
+        }
+
+        return ans;
+    }
+};
