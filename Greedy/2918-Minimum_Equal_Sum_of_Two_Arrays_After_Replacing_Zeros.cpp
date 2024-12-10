b9355548622c62cef27bd965681d3f87f0bba993
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-10 12:58:57 +0800

    Update 2918-Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros.cpp

--- Greedy/2918-Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros.cpp
+++ Greedy/2918-Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros.cpp
@@ -14,7 +14,6 @@ class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
         long long sum1 = 0, sum2 = 0;
-        long long cnt1 = 0, cnt2 = 0;
         long long zero1 = 0, zero2 = 0;
         for(int num : nums1) 
         {
