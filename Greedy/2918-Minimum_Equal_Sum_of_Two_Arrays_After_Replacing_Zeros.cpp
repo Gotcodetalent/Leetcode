f1dd7430bbbb957e8e1ee117153924a4fd528acc
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-10 12:58:48 +0800

    Update 2918-Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros.cpp

--- Greedy/2918-Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros.cpp
+++ Greedy/2918-Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros.cpp
@@ -10,3 +10,40 @@ https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-ze
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        long long sum1 = 0, sum2 = 0;
+        long long cnt1 = 0, cnt2 = 0;
+        long long zero1 = 0, zero2 = 0;
+        for(int num : nums1) 
+        {
+            if(num == 0) ++zero1;
+            else sum1 += num;
+        }
+        
+        for(int num : nums2) 
+        {
+            if(num == 0) ++zero2;
+            else sum2 += num;
+        }
+
+        sum1 += zero1;
+        sum2 += zero2;
+
+        if(sum1 > sum2)
+        {
+            if(zero2 == 0) return -1;
+            else return sum1;
+        }
+        else if(sum1 == sum2)
+        {
+            return sum1;
+        }
+        else
+        {
+            if(zero1 == 0) return -1;
+            else return sum2;
+        }
+    }
+};
