5a506231ed309c73b33a8875959b6e48d1192a46
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-10 16:43:33 +0800

    Update 0033-Search_in_Rotated_Sorted_Array.cpp

--- BS/0033-Search_in_Rotated_Sorted_Array.cpp
+++ BS/0033-Search_in_Rotated_Sorted_Array.cpp
@@ -3,10 +3,45 @@
 https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
 解說：
-
+先找到平移量 k, 接著BS, 以位移後的realmid查找target 
 
 有使用到的觀念：
 
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        const int n = nums.size();
+        int k = 0;
+        for(int i=0; i<n-1; ++i)
+        {
+            if(nums[i] > nums[i+1])
+            {
+                k = i+1;
+                break;
+            }
+        }
+        
+        int left = 0, right = n-1;
+
+        while(left <= right)
+        {
+            int mid = (left + (right - left)/2) % n;
+            int realmid = (mid + k)%n;
+            if(nums[realmid] < target)
+            {
+                left = mid + 1;
+            }
+            else if(nums[realmid] > target)
+            {
+                right = mid - 1;
+            }
+            else return realmid;
+        }
+
+
+        return -1;
+    }
+};
