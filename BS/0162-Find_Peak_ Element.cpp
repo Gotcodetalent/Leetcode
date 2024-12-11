9469d66842af50751726dad798df8b0fcfee41f9
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-11 18:25:15 +0800

    Update 0162-Find_Peak_ Element.cpp

--- BS/0162-Find_Peak_ Element.cpp
+++ BS/0162-Find_Peak_ Element.cpp
@@ -10,3 +10,30 @@ https://leetcode.com/problems/find-peak-element/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        return FindPeak(nums, 0, nums.size() -1);
+    }
+    int FindPeak(vector<int> nums, int left, int right){
+        
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            bool leftSmaller = (mid == 0 || nums[mid] > nums[mid-1]);
+            bool rightSmaller = (mid == nums.size()-1 || nums[mid] > nums[mid+1]);
+
+            if(leftSmaller && rightSmaller) return mid;
+
+            if(nums[mid] < nums[mid+1])
+            {
+                left = mid+1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
