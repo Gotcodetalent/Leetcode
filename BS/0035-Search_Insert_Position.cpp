855b945b6f0b3ac56e7d7c0102cd385baddfe492
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-11 15:34:57 +0800

    Update 0035-Search_Insert_Position.cpp

--- BS/0035-Search_Insert_Position.cpp
+++ BS/0035-Search_Insert_Position.cpp
@@ -10,3 +10,21 @@ https://leetcode.com/problems/search-insert-position/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        const int n = nums.size();
+        int left = 0, right = n-1;
+        while(left <= right)
+        {
+            int mid = left + (right-left)/2;
+            if(nums[mid] >= target) 
+            {
+                right = mid - 1;
+            }
+            else left = mid + 1;
+        }
+
+        return left;
+    }
+};
