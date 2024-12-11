3e4abc9e1298e82428860f94b2708c760c82beb2
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-11 16:18:08 +0800

    Update 0153-Find_Minimum_in_Rotated_Sorted_Array.cpp

--- BS/0153-Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ BS/0153-Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -10,3 +10,21 @@ https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        const int n = nums.size();
+        int left = 0, right = n-1;
+        while(left < right)
+        {
+            int mid = left + (right-left)/2;
+            if(nums[mid] > nums[right])
+            {
+                left = mid + 1;
+            }
+            else right = mid;
+        } 
+
+        return nums[left];
+    }
+};
