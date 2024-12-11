b1b299f1779f4ae8ad71ea2e8354b0b14d1b3791
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-11 19:37:59 +0800

    Update 0230-Kth_Smallest_Element_in_a_BST.cpp

--- BS/0230-Kth_Smallest_Element_in_a_BST.cpp
+++ BS/0230-Kth_Smallest_Element_in_a_BST.cpp
@@ -10,3 +10,23 @@ https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int order = 0;
+    int kthSmallest(TreeNode* root, int k) {
+        return findKth(root,k);
+    }
+
+    int findKth(TreeNode* root, int k)
+    {
+        int ret = INT_MAX/2;
+        if(root->left) ret = min(ret, findKth(root->left, k));
+                
+        ++order;
+        if(order == k) ret = root->val;
+        
+        if(root->right) ret = min(ret, findKth(root->right, k));
+
+        return ret;
+    }
+};
