91208b1529d0edbe7704d14e3019998db7617a77
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-11 21:01:07 +0800

    Update 0501-Find_Mode_in_Binary_Search_Tree.cpp

--- BS/0501-Find_Mode_in_Binary_Search_Tree.cpp
+++ BS/0501-Find_Mode_in_Binary_Search_Tree.cpp
@@ -10,3 +10,30 @@ https://leetcode.com/problems/find-mode-in-binary-search-tree/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        find(root);
+        return modes;
+    }
+    void find(TreeNode* root)
+    {
+        if(root->left) find(root->left);
+       
+        curr_count = curr_val == root->val ? curr_count+1 : 1;
+        if(curr_count == max_count) modes.push_back(root->val);
+        if(curr_count > max_count)
+        {
+            max_count = curr_count;
+            modes = {root->val};
+        }
+        curr_val = root->val;
+        
+        if(root->right) find(root->right);
+    }
+private:
+    int curr_count = 0;
+    int max_count = 0;
+    int curr_val = 0;
+    vector<int> modes;
+};
