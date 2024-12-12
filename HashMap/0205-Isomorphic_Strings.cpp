26ead7a1113a5d0c614da5236b52792576ee1830
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-12 23:00:01 +0800

    Update 0205-Isomorphic_Strings.cpp

--- HashMap/0205-Isomorphic_Strings.cpp
+++ HashMap/0205-Isomorphic_Strings.cpp
@@ -10,3 +10,27 @@ https://leetcode.com/problems/isomorphic-strings/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> ump;
+        unordered_map<char, int> used;
+        for(int i=0; i<t.length(); i++) 
+        {
+            if(ump.count(t[i])) continue;
+            else{
+                if(!used.count(s[i])){
+                    ump[t[i]] = s[i];    
+                    used[s[i]] = 1;
+                }
+            }
+        }
+
+        for(int i=0; i<t.length(); i++) 
+        {
+            if(ump[t[i]] != s[i]) return false;
+        }
+
+        return true;
+    }
+};
