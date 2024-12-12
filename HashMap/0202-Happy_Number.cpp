1ce35cbeb97b2e6df32c9407d3bc9f86f0d2a677
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-12 21:55:54 +0800

    Update 0202-Happy_Number.cpp

--- HashMap/0202-Happy_Number.cpp
+++ HashMap/0202-Happy_Number.cpp
@@ -11,3 +11,25 @@ https://leetcode.com/problems/happy-number/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_map<int,int> num;
+        while(n > 0)
+        {
+            if(num.count(n)) break;
+            num[n] = 1;
+            int tmp = 0, copy = n;            
+            while(copy > 0)
+            {
+                int r =  copy % 10;
+                tmp += r * r;
+                copy /= 10;
+            }
+            if(tmp == 1) return true;
+            n = tmp;
+        }
+
+        return false;
+    }
+};
