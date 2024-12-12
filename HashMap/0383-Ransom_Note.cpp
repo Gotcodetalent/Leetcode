1a2a72064088342d6c1a8ba399d7c131ebb8c604
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-12 23:58:10 +0800

    Update 0383-Ransom_Note.cpp

--- HashMap/0383-Ransom_Note.cpp
+++ HashMap/0383-Ransom_Note.cpp
@@ -10,3 +10,18 @@ https://leetcode.com/problems/ransom-note/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> ump;
+        for(char ch : magazine) ump[ch]++;
+        for(char ch : ransomNote) {
+            if(ump.count(ch)) --ump[ch];
+            else return false;
+
+            if(ump[ch] == 0) ump.erase(ch);
+        }
+
+        return true;
+    }
+};
