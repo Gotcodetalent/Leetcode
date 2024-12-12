dbc0dfb8e69af38a09ccc9c9f9e23a7f34cfa793
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-12 23:15:37 +0800

    Update 0242-Valid_Anagram.cpp

--- HashMap/0242-Valid_Anagram.cpp
+++ HashMap/0242-Valid_Anagram.cpp
@@ -10,3 +10,29 @@ https://leetcode.com/problems/valid-anagram/description/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> ump;
+        for(char ch : s) ump[ch]++;
+        for(char ch : t) {
+            if(ump.count(ch))
+            {
+                --ump[ch];
+                if(ump[ch] == 0) ump.erase(ch);
+            }   
+            else return false;
+        }
+
+        return ump.empty();
+    }
+};
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
