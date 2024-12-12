f60010d2865f1e4ba6f26484aef678e7796b90ca
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-13 00:04:42 +0800

    Update 0387-First_Unique_Character_in_a_String.cpp

--- HashMap/0387-First_Unique_Character_in_a_String.cpp
+++ HashMap/0387-First_Unique_Character_in_a_String.cpp
@@ -10,3 +10,20 @@ https://leetcode.com/problems/first-unique-character-in-a-string/description/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> ump;
+        for(int i=0; i<s.length(); i++)
+        {
+            ump[s[i]]++;
+        }
+        
+        for(int i=0; i<s.length(); i++)
+        {
+            if(ump[s[i]] == 1) return i;
+        }
+
+        return -1;
+    }
+};
