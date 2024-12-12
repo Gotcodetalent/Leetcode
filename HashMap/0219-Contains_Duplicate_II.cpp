fbd87db90ce4023f56498b3cccc904f700f69d4d
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-12 23:06:54 +0800

    Update 0219-Contains_Duplicate_II.cpp

--- HashMap/0219-Contains_Duplicate_II.cpp
+++ HashMap/0219-Contains_Duplicate_II.cpp
@@ -10,3 +10,19 @@ https://leetcode.com/problems/contains-duplicate-ii
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> ump;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(!ump.count(nums[i])) ump[nums[i]] = i;
+            else {
+                if(abs(ump[nums[i]] - i) <= k) return true;
+                ump[nums[i]] = i;
+            }
+        }
+
+        return false;
+    }
+};
