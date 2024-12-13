b52f4355fb2c5f06444e4e536eb2619d3afacf07
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-13 20:12:10 +0800

    Update 1436-Destination_City.cpp

--- HashMap/1436-Destination_City.cpp
+++ HashMap/1436-Destination_City.cpp
@@ -10,3 +10,20 @@ https://leetcode.com/problems/destination-city/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string, int> ump;
+        for(const auto& path : paths)
+        {
+            ump[path[0]]++;
+        }
+
+        for(const auto& path : paths)
+        {
+            if(!ump.count(path[1])) return path[1];
+        }
+
+        return "";
+    }
+};
