ce4f17262a5370f2f6212846b599fa55a15de44f
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-13 20:04:49 +0800

    Update 1424-Diagonal_Traverse_II.cpp

--- HashMap/1424-Diagonal_Traverse_II.cpp
+++ HashMap/1424-Diagonal_Traverse_II.cpp
@@ -11,3 +11,33 @@ https://leetcode.com/problems/diagonal-traverse-ii/description/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        unordered_map<int,int> ump;
+        vector<vector<int>> group;
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=0; j<nums[i].size(); j++)
+            {
+                if(ump.count(i+j)) group[i+j].push_back(nums[i][j]);
+                else{
+                    group.push_back({nums[i][j]});
+                    ump[i+j] = group.size() - 1;
+                }
+            }
+        }
+
+        vector<int> order;
+
+        for(const auto& vec : group)
+        {
+            for(int i=vec.size() - 1; i>=0 ; --i)
+            {
+                order.push_back(vec[i]);   
+            }
+        }
+        
+        return order;
+    }
+};
