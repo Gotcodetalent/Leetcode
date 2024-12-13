84cf97ad240db5d48898acda2611451564b7583a
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-13 19:35:25 +0800

    Update 1160-Find_Words_That_Can_Be_Formed_by_Characters.cpp

--- HashMap/1160-Find_Words_That_Can_Be_Formed_by_Characters.cpp
+++ HashMap/1160-Find_Words_That_Can_Be_Formed_by_Characters.cpp
@@ -10,3 +10,28 @@ https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        unordered_map<char,int> ump;
+        for(char ch : chars) ump[ch]++;
+
+        int good = 0;
+        for(string str : words) good += check(str, ump);
+
+        return good;
+    }
+
+    int check(string str, unordered_map<char,int> ump)
+    {
+        for(char ch : str)
+        {
+            if(!ump.count(ch)) return 0;
+            else --ump[ch];
+
+            if(ump[ch] == 0) ump.erase(ch);
+        }
+
+        return str.length();
+    }
+};
