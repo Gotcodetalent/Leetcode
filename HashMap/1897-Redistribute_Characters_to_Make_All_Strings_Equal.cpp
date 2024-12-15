153b6338a1ec1c5f504a24145b3c28d87d8d643d
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-16 00:29:41 +0800

    Update 1897-Redistribute_Characters_to_Make_All_Strings_Equal.cpp

--- HashMap/1897-Redistribute_Characters_to_Make_All_Strings_Equal.cpp
+++ HashMap/1897-Redistribute_Characters_to_Make_All_Strings_Equal.cpp
@@ -10,3 +10,24 @@ https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        unordered_map<char, int> ump;
+        for(string str : words)
+        {
+            for(char ch : str)
+            {
+                ump[ch]++;
+            }
+        }
+
+        int n = words.size();
+        for(auto& p : ump)
+        {
+            if((p.second % n) > 0) return false;
+        }
+
+        return true;
+    }
+};
