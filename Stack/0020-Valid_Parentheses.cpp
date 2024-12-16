a78946390d3e1a122d78f37339ee1e1c54c784d3
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-16 17:49:15 +0800

    Update 0020-Valid_Parentheses.cpp

--- Stack/0020-Valid_Parentheses.cpp
+++ Stack/0020-Valid_Parentheses.cpp
@@ -10,3 +10,33 @@ https://leetcode.com/problems/valid-parentheses/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char ch : s)
+        {
+            if(ch == '(' || ch == '{' || ch == '[') stk.push(ch);
+            else{
+                if(ch == ')')
+                {
+                    if(!stk.empty() && stk.top() == '(') stk.pop();
+                    else return false;
+                }
+                else if(ch == ']')
+                {
+                    if(!stk.empty() && stk.top() == '[') stk.pop();
+                    else return false;
+                }
+                else if(ch == '}')
+                {
+                    if(!stk.empty() && stk.top() == '{') stk.pop();
+                    else return false;
+                }
+            }
+        }
+
+        if(stk.empty()) return true;
+        else return false;
+    }
+};
