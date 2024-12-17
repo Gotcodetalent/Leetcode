29d1bcc73824ba2aba823b19960c1fd2855c8a42
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-17 12:45:50 +0800

    Update 0071-Simplify_Path.cpp

--- Stack/0071-Simplify_Path.cpp
+++ Stack/0071-Simplify_Path.cpp
@@ -10,3 +10,37 @@ https://leetcode.com/problems/simplify-path/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> stk;
+        string str = "";
+        for(int i=0; i<path.length(); i++)
+        {
+            if(path[i] == '/') continue;
+            string str = "";
+            while(i < path.size() && path[i] != '/')
+            {
+                str += path[i];
+                i++;
+            }
+
+            if(str == "." ) continue;
+            else if(str == ".."){
+                if(!stk.empty()) stk.pop();
+            }
+            else stk.push(str); 
+        }
+
+        string ans = "";
+
+        while(!stk.empty())
+        {
+            ans = "/" + stk.top() + ans;
+            stk.pop();
+        }
+        
+        if(ans == "") return "/";
+        else return ans;
+    }
+};
