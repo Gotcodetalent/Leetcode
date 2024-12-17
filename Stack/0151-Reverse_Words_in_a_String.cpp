fa03eedc304eacf2b00674dff220307f9cf051aa
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-17 13:19:09 +0800

    Update 0151-Reverse_Words_in_a_String.cpp

--- Stack/0151-Reverse_Words_in_a_String.cpp
+++ Stack/0151-Reverse_Words_in_a_String.cpp
@@ -10,3 +10,30 @@ https://leetcode.com/problems/reverse-words-in-a-string/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string> words;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s[i] == ' ') continue;
+            string str = "";
+            while(i < s.length() && s[i] != ' ')
+            {
+                str += s[i];
+                i++;
+            }
+            words.push(str);
+        }
+
+        string ans = words.top();
+        words.pop();
+        while(!words.empty())
+        {
+            ans = ans + " " + words.top();
+            words.pop();
+        }
+
+        return ans;
+    }
+};
