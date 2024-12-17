719894ec1c37559575f0cab038252b35df59a446
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-17 13:05:07 +0800

    Update 0150-Evaluate_Reverse_Polish_Notation.cpp

--- Stack/0150-Evaluate_Reverse_Polish_Notation.cpp
+++ Stack/0150-Evaluate_Reverse_Polish_Notation.cpp
@@ -10,3 +10,52 @@ https://leetcode.com/problems/evaluate-reverse-polish-notation/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> nums;
+        for(string str : tokens)
+        {
+            if(str != "+" && str != "-" && str != "*" && str != "/" ) nums.push(stoi(str));
+            else{
+                if(str == "+")
+                {
+                    int b = nums.top();
+                    nums.pop();
+                    int a = nums.top();
+                    nums.pop();
+                    int c = a + b;
+                    nums.push(c);
+                }
+                else if(str == "-")
+                {
+                    int b = nums.top();
+                    nums.pop();
+                    int a = nums.top();
+                    nums.pop();
+                    int c = a - b;
+                    nums.push(c);
+                }
+                else if(str == "*")
+                {
+                    int b = nums.top();
+                    nums.pop();
+                    int a = nums.top();
+                    nums.pop();
+                    int c = a * b;
+                    nums.push(c);
+                }
+                else{
+                    int b = nums.top();
+                    nums.pop();
+                    int a = nums.top();
+                    nums.pop();
+                    int c = a / b;
+                    nums.push(c);
+                }
+            }
+        }
+
+        return nums.top();
+    }
+};
