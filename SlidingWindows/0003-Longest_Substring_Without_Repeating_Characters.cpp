19fdf000a503223270e041fd1875383f7287f49e
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-18 20:21:30 +0800

    Update 0003-Longest_Substring_Without_Repeating_Characters.cpp

--- SlidingWindows/0003-Longest_Substring_Without_Repeating_Characters.cpp
+++ SlidingWindows/0003-Longest_Substring_Without_Repeating_Characters.cpp
@@ -11,3 +11,26 @@ https://leetcode.com/problems/longest-substring-without-repeating-characters/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> ump;
+        int head = 0;
+        int mx = 0;
+        for(int i=0; i<s.length(); i++)
+        {
+            ++ump[s[i]];
+            if(ump[s[i]] > 1)
+            {
+                while(ump[s[i]] != 1)
+                {
+                    --ump[s[head]];
+                    ++head;
+                }
+            }
+            mx = max(mx, i - head + 1);
+        }
+
+        return mx;
+    }
+};
