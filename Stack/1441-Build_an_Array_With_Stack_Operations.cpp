89a0c5fde75a80c88b63db59b68d4f203bbeb783
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-18 20:07:03 +0800

    Update 1441-Build_an_Array_With_Stack_Operations.cpp

--- Stack/1441-Build_an_Array_With_Stack_Operations.cpp
+++ Stack/1441-Build_an_Array_With_Stack_Operations.cpp
@@ -10,3 +10,32 @@ https://leetcode.com/problems/build-an-array-with-stack-operations/description/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        int curr = 0, stream = 1;
+        int size = target.size();
+        stack<int> stk;
+        vector<string> ops;
+        while(stream <= n)
+        {
+            if(curr == size) break;
+            if(stream == target[curr]) {
+                while(!stk.empty() && find(target.begin(), target.end(),stk.top()) == target.end()) {
+                    stk.pop();
+                    ops.push_back("Pop");
+                }
+                stk.push(stream);
+                ops.push_back("Push");
+                ++curr;
+            }
+            else {
+                stk.push(stream);
+                ops.push_back("Push");
+            }
+            ++stream;
+        }
+
+        return ops;
+    }
+};
