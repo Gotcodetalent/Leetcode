00eafed4b46aeb26a501305d9cfd735d715fddf5
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-20 11:48:30 +0800

    Update 0209-Minimum_Size_Subarray_Sum.cpp

--- SlidingWindows/0209-Minimum_Size_Subarray_Sum.cpp
+++ SlidingWindows/0209-Minimum_Size_Subarray_Sum.cpp
@@ -10,3 +10,23 @@ https://leetcode.com/problems/minimum-size-subarray-sum/description/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int l=0, r=0, minlen = INT_MAX;
+        while(r < nums.size())
+        {
+            target -= nums[r];
+            while(l <= r && target <= 0)
+            {
+                minlen = min(minlen, r-l+1);
+                target += nums[l];
+                ++l;
+            }
+
+            ++r;
+        }
+
+        return minlen == INT_MAX ? 0 : minlen;
+    }
+};
