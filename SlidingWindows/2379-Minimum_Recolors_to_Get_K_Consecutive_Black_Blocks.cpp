9b3dad8f4e465f2eac087793b1ff84481cb03b20
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-20 14:53:13 +0800

    Update 2379-Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp

--- SlidingWindows/2379-Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
+++ SlidingWindows/2379-Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
@@ -10,3 +10,30 @@ https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int black = 0, white = 0, op = INT_MAX;
+        int l = 0, r = 0;
+        while(r < blocks.length())
+        {
+            if(blocks[r] == 'W') ++white;
+            else ++black;
+
+            if(r - l + 1== k)
+            {
+                op = min(op, white);
+            }
+            else if(r - l + 1> k)
+            {
+                black = blocks[l] == 'B' ? black - 1 : black;
+                white = blocks[l] == 'W' ? white - 1 : white;
+                l++;
+                op = min(op, white);
+            }
+            ++r;
+        }
+
+        return op == INT_MAX ? 0 : op;
+    }
+};
