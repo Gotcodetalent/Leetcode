86a704cc712ec2eb7f88c0f4d6142ed7b270e44e
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-24 16:51:23 +0800

    Update 2957-Remove_Adjacent_Almost-Equal_Characters.cpp

--- SlidingWindows/2957-Remove_Adjacent_Almost-Equal_Characters.cpp
+++ SlidingWindows/2957-Remove_Adjacent_Almost-Equal_Characters.cpp
@@ -4,7 +4,8 @@ https://leetcode.com/problems/remove-adjacent-almost-equal-characters/
 
 解說：
 找出讓 word 不會產生 "相鄰字母相同 or 相鄰字母在字母順序上是連續的" 情況, 所需要的最小步驟
-這裡使用dp, 從左右各掃一次看哪一邊需要的步驟最少
+以從左邊開始為例, 當我們調整了一個位置 pos 的字母時, pos + 1 的左側便不必再檢查 (line 24: i++) 
+從左右各掃一次看哪一邊需要的步驟最少
 
 有使用到的觀念：
 
