e1e5f4b531a03dea384dee285445000657ff12ff
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-25 10:18:46 +0800

    Update 0009-Palindrome_Number.cpp

--- Math/0009-Palindrome_Number.cpp
+++ Math/0009-Palindrome_Number.cpp
@@ -1,6 +1,6 @@
 /*
 題目：
-
+https://leetcode.com/problems/palindrome-number/
 
 解說：
 
@@ -10,3 +10,16 @@
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string integer = to_string(x);
+        int size = integer.length();
+        for(int i=0; i<size/2; i++)
+        {
+            if(integer[i] != integer[size-i-1]) return false;
+        }
+
+        return true;
+    }
+};
