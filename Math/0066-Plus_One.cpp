2448ae7b7dd1522ac091035f7167d45edbd7c8f4
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-25 11:37:33 +0800

    Update 0066-Plus_One.cpp

--- Math/0066-Plus_One.cpp
+++ Math/0066-Plus_One.cpp
@@ -10,3 +10,26 @@ https://leetcode.com/problems/plus-one/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        const int n = digits.size();
+        digits[n-1]++;
+        for(int i=n-1; i>=1; i--)
+        {
+            if(digits[i] > 9)
+            {
+                digits[i] %= 10;
+                digits[i-1]++;
+            }
+        }
+
+        if(digits[0] > 9)
+        {
+            digits.insert(digits.begin(),1);
+            digits[1] %= 10;
+        }
+
+        return digits;
+    }
+};
