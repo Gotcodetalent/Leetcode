7986fec076dea28d754021696c79b8306362d199
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-26 19:49:21 +0800

    Update 0190-Reverse_Bits.cpp

--- Math/0190-Reverse_Bits.cpp
+++ Math/0190-Reverse_Bits.cpp
@@ -10,3 +10,15 @@ https://leetcode.com/problems/reverse-bits/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        for(uint32_t i=0; i<32; i++)
+        {
+            if((n >> i)&1) ans += pow(2,31-i);
+        }
+
+        return ans;
+    }
+};
