2714669960457ebc1af56caf326569bad6916417
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-12-26 22:15:39 +0800

    Update 0342-Power_of_Four.cpp

--- Math/0342-Power_of_Four.cpp
+++ Math/0342-Power_of_Four.cpp
@@ -3,7 +3,9 @@
 https://leetcode.com/problems/power-of-four/
 
 解說：
-
+n & (n-1) == 0 用於確認是否為 2 的倍數
+0x55555555 -> 0101 0101 0101 0101 0101 0101 0101 0101
+因為是四的倍數，因此只會在偶數位置上會是 1 奇數位置上為 0
 
 有使用到的觀念：
 
@@ -18,5 +20,13 @@ public:
         }
 
         return n == 1;
+    }
+};
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) 
+    {
+        return n > 0 && ((n & (n-1)) == 0) && (n & 0x55555555);
     }
 };
