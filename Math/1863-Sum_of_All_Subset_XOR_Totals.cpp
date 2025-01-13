bc13fdf10e8969b43683a209a6c05fb545445907
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2025-01-13 11:19:32 +0800

    Update 1863-Sum_of_All_Subset_XOR_Totals.cpp

--- Math/1863-Sum_of_All_Subset_XOR_Totals.cpp
+++ Math/1863-Sum_of_All_Subset_XOR_Totals.cpp
@@ -10,3 +10,27 @@ https://leetcode.com/problems/sum-of-all-subset-xor-totals/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    int ans;
+    int subsetXORSum(vector<int>& nums) {
+        ans = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            recursive(nums,i,0);
+        }
+
+        return ans;
+    }
+
+    void recursive(vector<int>& nums, int start, int val)
+    {
+        if(start >= nums.size()) return;
+        val ^= nums[start];
+        ans += val;
+        for(int i=start+1; i<nums.size(); i++)
+        {
+            recursive(nums, i, val);
+        }
+    }
+};
