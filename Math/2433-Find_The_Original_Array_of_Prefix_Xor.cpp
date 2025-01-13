9636ced8b9a7e84b1ad1f5b3e34b06eb33744844
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2025-01-13 13:07:21 +0800

    Update 2433-Find_The_Original_Array_of_Prefix_Xor.cpp

--- Math/2433-Find_The_Original_Array_of_Prefix_Xor.cpp
+++ Math/2433-Find_The_Original_Array_of_Prefix_Xor.cpp
@@ -10,3 +10,18 @@ https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        int curr_Xor = 0;
+        vector<int> ans;
+        for(int i=0; i<pref.size(); i++)
+        {
+            int original = curr_Xor ^ pref[i];
+            ans.push_back(original);
+            curr_Xor ^= original;
+        }
+
+        return ans;
+    }
+};
