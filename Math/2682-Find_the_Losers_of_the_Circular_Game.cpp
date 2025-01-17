8e9091bcf22a01477e4b6f1abdc494fe0f05b9b8
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2025-01-17 16:41:37 +0800

    Update 2682-Find_the_Losers_of_the_Circular_Game.cpp

--- Math/2682-Find_the_Losers_of_the_Circular_Game.cpp
+++ Math/2682-Find_the_Losers_of_the_Circular_Game.cpp
@@ -3,6 +3,11 @@
 https://leetcode.com/problems/find-the-losers-of-the-circular-game/description/
 
 解說：
+Rule 
+1. 遊戲開始時從第 1 個人開始傳
+2. 每 round 都要傳遞給 next (round * k) step 的人
+3. 有人收到 2 次球遊戲便結束 (第 1 個人剛開始那次要算進去)
+4. 遊戲結束時沒收過球的為 loser
 
 
 有使用到的觀念：
@@ -10,3 +15,26 @@ https://leetcode.com/problems/find-the-losers-of-the-circular-game/description/
 */
 
 // #include "../code_function.h"
+class Solution {
+public:
+    vector<int> circularGameLosers(int n, int k) {
+        vector<int> record(n);
+        record[0] = 1;
+        int round = 0, receiver = 0;
+        while(1)
+        {
+            ++round;
+            receiver = (receiver + round*k) % n;
+            record[receiver]++;
+            if(record[receiver] == 2) break;
+        }
+
+        vector<int> loser;
+        for(int i=0; i<n; i++)
+        {
+            if(record[i] == 0) loser.push_back(i+1);
+        }
+
+        return loser;
+    }
+};
