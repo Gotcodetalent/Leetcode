8bfc0b92ec4700825b613fbc3c112e2a89c8ab87
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2025-02-26 15:32:57 +0800

    Update 1749-Maximum_Absolute_Sum_Of_Any_Subarray.cpp

--- Array/1749-Maximum_Absolute_Sum_Of_Any_Subarray.cpp
+++ Array/1749-Maximum_Absolute_Sum_Of_Any_Subarray.cpp
@@ -4,6 +4,11 @@ https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/description/
 
 解說：
 
+核心概念同 Kadane’s Algorithm，但要同時追蹤：
+
+1. 最大子陣列和（max_sum）：Kadane's Algorithm所求，可以找出最大正數和的子陣列。
+2. 最小子陣列和（min_sum）：一樣是 Kadane’s Algorithm，但找的是最小和（最大負數和）
+答案即 max(max_sum, abs(min_sum))。
 
 有使用到的觀念：
 
