12a99f8f8602a430811029cf120b63217ca09ed8
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2025-03-31 09:48:32 +0800

    Create Find_All_Possible_Recipes_from_Given_Supplies.cpp

---
+++ BFS/Find_All_Possible_Recipes_from_Given_Supplies.cpp
@@ -0,0 +1,141 @@
+/*
+題目：
+https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/description/
+
+解說：
+1. BFS
+2. DFS
+
+有使用到的觀念：
+
+*/
+
+// #include "../code_function.h"
+## BFS:
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_map<string, int> inDegree; // 記錄每道食譜還需要多少原料
+        unordered_map<string, vector<string>> graph; // 記錄某個原料可解鎖的食譜
+        unordered_set<string> supplySet(supplies.begin(), supplies.end());
+        queue<string> q;
+        vector<string> ans;
+
+        // 1. 建立 indegree table & dependency graph
+        for (int i = 0; i < recipes.size(); i++) {
+            inDegree[recipes[i]] = ingredients[i].size(); // 這道食譜需要的原料數量
+            for (const string& ing : ingredients[i]) {
+                graph[ing].push_back(recipes[i]); // 當 `ing` 可用時，可解鎖 `recipes[i]`
+            }
+        }
+
+        // 2. 將現有 `supplies` 放入 queue
+        for (const string& s : supplies) {
+            q.push(s);
+        }
+
+        // 3. 拓撲排序 (BFS)
+        while (!q.empty()) {
+            string item = q.front();
+            q.pop();
+
+            // 若該 item 是食譜 (存在於 inDegree)，則加入答案
+            if (inDegree.count(item)) {
+                ans.push_back(item);
+            }
+
+            // 該 item 是某些食譜的原料，減少對應食譜的 indegree
+            for (const string& recipe : graph[item]) {
+                if (--inDegree[recipe] == 0) { // 當該食譜 indegree 變為 0，表示可以製作
+                    q.push(recipe);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+## DFS:
+class Solution {
+public:
+    unordered_map<string, bool> complete;
+    unordered_set<string> supplySet;
+    unordered_set<string> recipeSet;
+    unordered_map<string, int> recipeIndex;
+    unordered_set<string> visited;
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        vector<string> ans;
+        supplySet = unordered_set<string>(supplies.begin(), supplies.end());
+        recipeSet = unordered_set<string>(recipes.begin(), recipes.end());
+
+        for(int i = 0; i < recipes.size(); ++i)
+        {
+            recipeIndex[recipes[i]] = i;
+        }
+
+        for(int i = 0; i < recipes.size(); ++i)
+        {
+            if(check(i, recipes, ingredients, supplies))
+            {
+                ans.push_back(recipes[i]);
+            }
+        }    
+
+        return ans;
+    }
+private:
+    bool check(int index, vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies)
+    {
+        if(complete.count(recipes[index])) return true;
+        if(visited.count(recipes[index])) return false;
+
+        visited.insert(recipes[index]);
+
+        for(int i = 0; i < ingredients[index].size(); ++i)
+        {
+            if(inRecipe(ingredients[index][i]))
+            {
+                int location = findIndex(ingredients[index][i]);
+                if(!check(location ,recipes, ingredients, supplies))
+                {
+                    return false;
+                }
+            }
+            else if(!inSupplies(ingredients[index][i]))
+            {
+                return false;
+            }
+        }
+        
+        complete[recipes[index]] = true;
+        return true;
+    }
+
+    int findIndex(string& ingredient)
+    {
+        return recipeIndex[ingredient];
+    }
+
+    bool inRecipe(string& ingredient)
+    {
+        if(recipeSet.count(ingredient))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    bool inSupplies(string& ingredient)
+    {
+        if(supplySet.count(ingredient))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+};
